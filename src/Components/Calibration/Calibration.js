2544d039f960b25d90a0f0aad2f3a9cba1704703
ebhoom1 <info.ebhoom@gmail.com>
2024-04-17 20:31:53 +0530

    front end connection started in Calibration

--- src/Components/Calibration/Calibration.js
+++ src/Components/Calibration/Calibration.js
@@ -1,16 +1,127 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import LeftSideBar from "../LeftSideBar/LeftSideBar";
-
+import { ToastContainer, toast } from 'react-toastify';
 import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { useEffect } from 'react';
 import CalibrationData from './Calibration-Data';
+import axios from 'axios';
 
 
 
 const Calibration = () => { 
+  const [validUserData, setValidUserData] = useState(null);
+  const [calibrationData,setCalibrationData]=useState({
+    userId:"",
+    date:"",
+    userName:"",
+    equipmentName:"",
+    before:"",
+    after:"",
+    technician:"",
+    notes:"",
+  })
+  const handleInputChange = event =>{
+    const[name,value]=event.target;
+    setCalibrationData({
+      ...calibrationData,
+      [name]:value,
+    });
+  }
+  const handleSubmit = async (event)=>{
 
+      try {
+        event.preventDefault();
+        
+        if(calibrationData.userId === ''){
+          toast.warning('Please add the User ID',{
+            position:'top-center'
+          })
+        }else if(calibrationData.date === ''){
+          toast.warning('Please add the date',{
+            position:'top-center'
+          })
+        }else if(calibrationData.equipmentName === ''){
+          toast.warning('Please add the equipment Name',{
+            position:"top-center"
+          })
+        }else if(calibrationData.userName === ''){
+          toast.warning('Please add user Name ',{
+            position:"top-center"
+          })
+        }else if(calibrationData.technician === ''){
+          toast.warning('Please add the technician',{
+            position:"top-center"
+          })
+        }else if(calibrationData.after === ''){
+          toast.warning('Please add the after',{
+            position:'top-center'
+          })
+        }else{
+          let calibrationDataToSend=calibrationData;
+
+          const res = await axios.post('http://localhost:4444/api/add-calibration',calibrationDataToSend)
+          console.log('calibrationDataToSend',calibrationDataToSend);
+          if(res.status === '201'){
+            const shouldSaveIt = window.confirm("Are you Sure to Save the User?")
+
+            if(shouldSaveIt){
+              console.log('Data submitted successfully');
+
+              setCalibrationData({
+                userId:"",
+                date:"",
+                userName:"",
+                equipmentName:"",
+                before:"",
+                after:"",
+                technician:"",
+                notes:"",
+              })
+            }
+          }
+          toast.success('The Calibration  is added Successfully',{
+            position:'top-center'
+          })
+        }
+      } catch (error) {
+        console.log(error);
+        toast.error('Error In Occured Please try again',{
+          position:'top-center'
+        })
+      }
+  }
+  useEffect(()=>{
+    //Fetch product iD and user status when the component mounts
+    
+    const fetchData=async()=>{
+      try{
+          let token = localStorage.getItem("userdatatoken")
+          const response =await axios.get('http://localhost:4444/api/validuser',{
+            headers:{
+              'Content-Type':"application/json",
+              'Authorization':token,
+              Accept:'application/json'
+            },
+            withCredentials: true
+          })
+          const data = response.data;
+        console.log(data);
+
+        if (data.status === 201) {
+          // Update product ID and user status
+          setValidUserData(data.validUserOne);
+          console.log(data.validUserOne);
+        } else {
+          console.log("Error fetching user data");
+        }
+      }catch(error){
+        console.error("Error fetching user data :", error);
+      }
+    }
+    fetchData();
+  },[])
     return (
       <div className="main-panel">
         <div className="content-wrapper">
@@ -55,7 +166,12 @@ const Calibration = () => {
                              {/* <h1>Update User</h1> */}
                           </div>
 
-                          
+                          <div className="col-12 col-lg-6 col-md-6 mb-3">
+                            <label htmlFor="exampleFormControlInput5">User ID</label>
+                            <input type="text" className="form-control" id="exampleFormControlInput5" placeholder="Equipment Name" value= { validUserData && validUserData._id}
+                            />
+                            
+                          </div>
 
                           <div className="col-12 col-lg-6 col-md-6 mb-3">
                             <label htmlFor="exampleFormControlInput4">Date of Calibration</label>
