cb0e4afaf8c381648dd29fcf6adad63b0a0e0203
ebhoom1 <info.ebhoom@gmail.com>
2024-04-21 17:45:47 +0530

    edit in Account Page

--- src/Components/Account/Account.js
+++ src/Components/Account/Account.js
@@ -2,6 +2,7 @@ import React, { Component, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import LeftSideBar from "../LeftSideBar/LeftSideBar";
 import axios from 'axios';
+import './index.css'
 
 const  Account=()=>{
   const [validUserData, setValidUserData] = useState(null);
@@ -70,12 +71,16 @@ const  Account=()=>{
                       <p className="account-details">User ID : { validUserData && validUserData.userName} <sup><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check-circle-fill" viewBox="0 0 16 16">
                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
                       </svg></sup></p>
+                      <p className="account-details">Company Name : { validUserData && validUserData.companyName}</p>
                       <p className="account-details">Model Name : AquaBox Model M</p>
+                      <p className="account-details">Name : { validUserData && validUserData.fname}</p>
                       <p className="account-details">Email ID :  { validUserData && validUserData.email}</p>
-                      <p className="account-details">Password : ************</p>
-                      <p className="account-details">User Type : { validUserData && validUserData.userType}</p>
+                      <p className="account-details">Password : ************  <Link to='/reset-password-email'> <button type="button" className="password-button">Change Password</button></Link></p>
+                     
+                      <p className="account-details">Subcription Date : { validUserData && validUserData.subscriptionDate}</p>
+                      <p className="account-details">Industry Type : { validUserData && validUserData.industryType}</p>
 
-                     <Link to='/reset-password-email'> <button type="button" className="password-button">Change Password</button></Link>
+                    
 
                     </div>
                   </div>
