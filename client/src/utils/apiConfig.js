68b67fc0e5439d6295a3a113cf1ab6979234cb32
Mohamed Fazil M <info.ebhoom@gmail.com>
2024-06-20 22:07:41 +0530

    changed the url

--- client/src/utils/apiConfig.js
+++ client/src/utils/apiConfig.js
@@ -1,4 +1,4 @@
-export const API_URL = 'http://13.232.163.105:5555'
+export const API_URL = 'http://65.0.29.83:5555'
 export const LOCAL_API_URL = 'http://localhost:5555' 
 
 //'http://13.202.11.195:5555'
