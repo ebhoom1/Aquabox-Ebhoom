1b33939e6faac72443ec32e456044f58c64d5212
Mohamed Fazil M <info.ebhoom@gmail.com>
2024-06-21 10:37:24 +0530

    api url edit

--- client/src/utils/apiConfig.js
+++ client/src/utils/apiConfig.js
@@ -1,4 +1,4 @@
-export const API_URL = 'http://3.110.90.123:5555'
+export const API_URL = 'http://3.108.59.97:5555'
 export const LOCAL_API_URL = 'http://localhost:5555' 
 
 //'http://13.202.11.195:5555'
