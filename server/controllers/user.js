178fee5c9b43c260545291b411013fe6d6f4afd9
Mohamed Fazil M <info.ebhoom@gmail.com>
2024-06-21 14:34:33 +0530

    edit in url of sendpasswordlink

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -156,7 +156,7 @@ const sendPasswordLink= async (req,res)=>{
                 from:process.env.EMAIl,
                 to:email,
                 subject:"Sending Email for Password Reset",
-                text:`This Link Valid for 2 Minutes http://3.110.90.123:5555/reset-password/${userfind._id}/${setusertoken.verifytoken}`
+                text:`This Link Valid for 2 Minutes http://13.127.99.126:5555/reset-password/${userfind._id}/${setusertoken.verifytoken}`
 
             }
             transporter.sendMail(mailOptions,(error,info)=>{
