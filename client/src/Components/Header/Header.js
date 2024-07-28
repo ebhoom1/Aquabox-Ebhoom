273e66f0f5cbcefd20d0021c623bb71cbd742fcd
fazilmm860 <fazilmm860@gmail.com>
2024-07-29 03:06:45 +0530

    bug fixed in header page

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -7,7 +7,7 @@ export default class Header extends Component {
     return (
         <nav className="navbar navbar-expand-lg navbar-light ">
         <div className="container">
-         <Link className='link' to="/">  <img src="/assets/images/logo.png" className="footer-img" /></Link>
+         <Link className='link' to="/account">  <img src="/assets/images/logo.png" className="footer-img" /></Link>
 
          <Link className="link" to="/download-IoT-Data"><button className='signup-button'>Download Data</button></Link>
          
