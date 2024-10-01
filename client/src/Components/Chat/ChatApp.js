6f95da49c71a4f7a75efb364e69acea18d74ff59
ebhoom1 <info.ebhoom@gmail.com>
2024-10-01 09:16:00 +0530

    backend also SSL enabled

--- client/src/Components/Chat/ChatApp.js
+++ client/src/Components/Chat/ChatApp.js
@@ -7,7 +7,7 @@ import { useSelector } from 'react-redux';
 import './chat.css'; // Ensure this path is correct
 import { API_URL } from '../../utils/apiConfig'; // Ensure API_URL is correct
 
-const socket = io(`http://13.233.106.171:5555`, { withCredentials: true });
+const socket = io(`${API_URL}`, { withCredentials: true });
 
 const ChatApp = () => {
   const [chats, setChats] = useState([]);
