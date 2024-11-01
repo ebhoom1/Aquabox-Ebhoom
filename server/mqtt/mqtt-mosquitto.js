13f8db3f2868a55c954c18c251bcc0b9d52173d5
ebhoom1 <info.ebhoom@gmail.com>
2024-11-01 19:42:17 +0530

    exceedanceColor is and exceedanceComment added

--- server/mqtt/mqtt-mosquitto.js
+++ server/mqtt/mqtt-mosquitto.js
@@ -98,8 +98,8 @@ const setupMqttClient = (io) => {
                     date: moment().format('DD/MM/YYYY'),
                     time: moment().format('HH:mm'),
                     timestamp: new Date(),
-                    ExceedanceColor: 'yellow', // Default value
-                exceedanceComment: 'No exceedance detected from mqtt' // Default value
+                    ExceedanceColor: 'green', // Default value
+                exceedanceComment: 'No exceedance detected' // Default value
                 };
 
                 await axios.post('https://api.ocems.ebhoom.com/api/handleSaveMessage', payload);
