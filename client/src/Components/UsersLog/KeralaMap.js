61d2d45203b317a2236000588a60356819910d43
ebhoom1 <info.ebhoom@gmail.com>
2024-11-02 11:58:00 +0530

    Added the StackData in the Map onClick It will show the realTime data

--- client/src/Components/UsersLog/KeralaMap.js
+++ client/src/Components/UsersLog/KeralaMap.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchLatestIotData } from "../../redux/features/iotData/iotDataSlice";
+import { fetchUserLatestByUserName } from "../../redux/features/userLog/userLogSlice";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
 const KeralaMap = ({ users }) => {
   const dispatch = useDispatch();
-  const latestIotData = useSelector((state) => state.iotData.latestData);
+  const latestUser = useSelector((state) => state.userLog.latestUser);
   const [selectedUser, setSelectedUser] = useState(null);
 
   const defaultPosition = [10.8505, 76.2711]; // Center position of Kerala
@@ -32,26 +32,8 @@ const KeralaMap = ({ users }) => {
 
   const handleMarkerClick = (user) => {
     setSelectedUser(user.userName);
-    dispatch(fetchLatestIotData(user.userName));
+    dispatch(fetchUserLatestByUserName(user.userName));
   };
-
-  // Fields to be excluded from the display
-  const excludedFields = [
-    "_id",
-    "product_id",
-    "userName",
-    "companyName",
-    "industryType",
-    "mobileNumber",
-    "email",
-    "time",
-    "date",
-    "topic",
-    "validationStatus",
-    "validationMessage",
-    "timestamp",
-    "__v"
-  ];
 
   return (
     <MapContainer center={defaultPosition} zoom={7} style={{ height: "500px", width: "100%" }}>
@@ -59,53 +41,62 @@ const KeralaMap = ({ users }) => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.ebhoom.com/">Ebhoom Solutions</a> contributors'
       />
-      {users
-        .filter(user => user.userType === "user")  // Only show users with userType "user"
-        .map((user) => {
-          const userIoT = selectedUser === user.userName ? latestIotData : null;
-          const isHealthy = userIoT && userIoT.validationStatus === "Valid";
-          const analyzerHealth = userIoT && userIoT.validationMessage ? userIoT.validationMessage : isHealthy ? "Good" : "Problem";
+      {users && users.length > 0 &&
+        users
+          .filter((user) => user.userType === "user")
+          .map((user) => {
+            const userIoT = selectedUser === user.userName && latestUser ? latestUser : null;
+            const isHealthy = userIoT && userIoT.validationStatus === "Valid";
+            const analyzerHealth = userIoT?.validationMessage || (isHealthy ? "Good" : "Problem");
 
-          return (
-            <Marker
-              key={user._id}
-              position={[user.latitude, user.longitude]}
-              icon={isHealthy ? greenIcon : redIcon}
-              eventHandlers={{
-                click: () => handleMarkerClick(user),
-              }}
-            >
-              <Popup>
-                <div>
-                  <h5>User ID: {user.userName}</h5>
-                  <p>Company Name: <strong>{user.companyName}</strong></p>
-                  <p>Model Name: <strong>{user.modelName}</strong></p>
-                  <p>Analyzer Health: <strong>{analyzerHealth}</strong></p>
-                  {userIoT && (
-                    <div style={styles.scrollContainer}>
-                      <div style={styles.cardContainer}>
-                        {Object.entries(userIoT)
-                          .filter(([key]) => !excludedFields.includes(key))
-                          .map(([key, value]) => (
-                            <div key={key} style={styles.card}>
-                              <strong>{key}:</strong> <p>{value !== null ? value : "N/A"}</p>
+            return (
+              <Marker
+                key={user._id}
+                position={[user.latitude, user.longitude]}
+                icon={isHealthy ? greenIcon : redIcon}
+                eventHandlers={{
+                  click: () => handleMarkerClick(user),
+                }}
+              >
+                <Popup maxWidth={500} minWidth={300}>
+                  <div style={styles.popupContainer}>
+                    <h5>User ID: {user.userName}</h5>
+                    <p>Company Name: <strong>{user.companyName}</strong></p>
+                    <p>Analyzer Health: <strong>{analyzerHealth}</strong></p>
+                    {userIoT && (
+                      <div style={styles.scrollContainer}>
+                        <div style={styles.cardContainer}>
+                          {userIoT.stackData.map((stack) => (
+                            <div key={stack._id} style={styles.stackContainer}>
+                              <h6 className="text-center">{stack.stackName}</h6>
+                              {Object.entries(stack).map(([key, value]) => (
+  key !== "_id" && key !== "stackName" && ( // Exclude "_id" and "stackName"
+    <div key={key} style={styles.valueCard}>
+      <strong>{key}:</strong> {value !== null ? value : "N/A"}
+    </div>
+  )
+))}
                             </div>
                           ))}
+                        </div>
                       </div>
-                    </div>
-                  )}
-                </div>
-              </Popup>
-            </Marker>
-          );
-        })}
+                    )}
+                  </div>
+                </Popup>
+              </Marker>
+            );
+          })}
     </MapContainer>
   );
 };
 
 const styles = {
+  popupContainer: {
+    maxWidth: "100%", // Adjust popup width
+    overflow: "auto",
+  },
   scrollContainer: {
-    maxHeight: "200px", // Limit the height of the scrollable area
+    maxHeight: "400px", // Increase the height of the scrollable area
     overflowY: "auto",  // Enable vertical scrolling
   },
   cardContainer: {
@@ -114,12 +105,19 @@ const styles = {
     gap: "10px",
     justifyContent: "center",
   },
-  card: {
+  stackContainer: {
+    width: "100%",
+    padding: "10px",
+    borderBottom: "1px solid #dee2e6",
+    textAlign: "center",
+  },
+  valueCard: {
     backgroundColor: "#f8f9fa",
     border: "1px solid #dee2e6",
     borderRadius: "5px",
     padding: "10px",
-    width: "120px",
+    margin: "5px 0",
+    width: "100%", // Full width for each value card
     textAlign: "center",
     boxShadow: "0 1px 3px rgba(0, 0, 0, 0.1)",
   },
